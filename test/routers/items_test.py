e1ab84e0539423f353e31e65a719cf2e98dcc4af
coroo <coroo.wicaksono@gmail.com>
2020-10-08 16:20:50 +0700

    :construction_worker: add unit testing

---
+++ test/routers/items_test.py
@@ -0,0 +1,57 @@
+from fastapi.testclient import TestClient
+from env import settings
+from faker import Faker
+
+from main import app
+
+client = TestClient(app)
+fake = Faker()
+
+fake_name = fake.name()
+fake_description = fake.address()
+
+
+# def test_read_item():
+#     response = client.get(settings.API_PREFIX+"/items/1", headers={"X-Token": "fake-super-secret-token"})
+#     assert response.status_code == 200
+    # assert response.json() == {
+    #     "id": "foo",
+    #     "title": "Foo",
+    #     "description": "There goes my hero",
+    # }
+    
+def test_create_item():
+    response = client.post(
+        settings.API_PREFIX+"/users/1/items/",
+        headers={"X-Token": "fake-super-secret-token"},
+        json={"title": fake_name, "description": fake_description},
+    )
+    assert response.status_code == 200, response.text
+
+    # data = response.json()
+    # assert data["title"] == fake_name
+    # assert data["description"] == fake_description
+    # assert "id" in data
+    # item_id = data["id"]
+
+    # response = client.get(f"{settings.API_PREFIX}/items/{item_id}")
+    # assert response.status_code == 200, response.text
+    # data = response.json()
+    # assert data["title"] == fake_name
+    # assert data["description"] == fake_description
+    # assert data["id"] == item_id
+
+def test_read_items():
+    response = client.get(settings.API_PREFIX+"/items/", headers={"X-Token": "fake-super-secret-token"})
+    assert response.status_code == 200
+    # assert response.json() == {"detail": "Not Found"}
+
+# def test_read_user():
+#     response = client.get(settings.API_PREFIX+"/items/1")
+#     assert response.status_code == 200
+#     # assert response.json() == {"detail": "Not Found"}
+
+# def test_read_user_doesnt_exist():
+#     response = client.get(settings.API_PREFIX+"/items/987654321")
+#     assert response.status_code == 404
+#     # assert response.json() == {"detail": "Not Found"}
--- test/routers/users_test.py
+++ test/routers/users_test.py
@@ -1,11 +1,50 @@
 from fastapi.testclient import TestClient
+from env import settings
+from faker import Faker
 
 from main import app
 
 client = TestClient(app)
+fake = Faker()
 
+fake_email = fake.email()
+fake_password = fake.password()
 
-def test_read_main():
-    response = client.get("/")
+def test_create_user():
+    response = client.post(
+        settings.API_PREFIX+"/users/",
+        json={"email": fake_email, "password": fake_password},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["email"] == fake_email
+    assert "id" in data
+    user_id = data["id"]
+
+    response = client.get(f"{settings.API_PREFIX}/users/{user_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["email"] == fake_email
+    assert data["id"] == user_id
+
+def test_create_user_duplicate():
+    response = client.post(
+        settings.API_PREFIX+"/users/",
+        json={"email": fake_email, "password": fake_password},
+    )
+    assert response.status_code == 400, response.text
+
+def test_read_users():
+    response = client.get(settings.API_PREFIX+"/users/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    # assert response.json() == {"detail": "Not Found"}
+
+def test_read_user():
+    response = client.get(settings.API_PREFIX+"/users/1")
+    assert response.status_code == 200
+    # assert response.json() == {"detail": "Not Found"}
+
+def test_read_user_doesnt_exist():
+    response = client.get(settings.API_PREFIX+"/users/987654321")
+    assert response.status_code == 404
+    # assert response.json() == {"detail": "Not Found"}
