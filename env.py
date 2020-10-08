3fc5f4425af03821679816e9300a2c0990a5a3a5
Alfiansyah Ichsan <alfiansyah.ichsan@gmail.com>
2020-10-08 14:22:06 +0700

    add cors, logging, status code, edit readme

--- env.py
+++ env.py
@@ -1,11 +1,12 @@
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
-    ## APPLICATION SETTING
+    # APPLICATION SETTING
     APP_NAME: str = "Awesome Base Architecture"
     API_PREFIX: str = "/api/v1"
 
-    ## DB CONNECTION
+    # DB CONNECTION
     DB_CONNECTION: str = "mysql"
     DB_PORT: int = 3306
     DB_HOST: str = "localhost"
@@ -13,7 +14,11 @@ class Settings(BaseSettings):
     DB_USERNAME: str = "root"
     DB_PASSWORD: str = "root_pass"
 
+    # ALLOWED ORIGINS
+    ALLOWED_ORIGINS: str = "http://localhost:8080,http://127.0.0.1:8080"
+
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
