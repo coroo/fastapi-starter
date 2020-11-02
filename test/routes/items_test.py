fd3936b7a923aac0edd370fae4ddadbc6c9d0f82
coroo <coroo.wicaksono@gmail.com>
2020-11-02 15:38:11 +0700

    :zip: add unit test documentation

--- test/routes/items_test.py
+++ test/routes/items_test.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from env import settings
 from faker import Faker
+import pytest
 
 from main import app
 
@@ -13,77 +14,95 @@ fake_name_2 = fake.name()
 fake_description = fake.address()
 
 
-def test_func_items():
-    response = client.post(
-        settings.API_PREFIX+"/users/token",
-        data={"username": "coroo.wicaksono@gmail.com",
-              "password": "mysecretpass"},
-    )
+class TestItems():
+    @pytest.fixture(autouse=True)
+    def _setup(self):
+        # REQUEST AND TOKEN SETUP
+        response = client.post(
+            settings.API_PREFIX+"/users/token",
+            data={"username": "coroo.wicaksono@gmail.com",
+                  "password": "mysecretpass"},
+        )
 
-    assert response.status_code == 200, response.text
-    session_data = response.json()
-    assert session_data['token_type'] is not None
-    assert session_data['access_token'] is not None
+        assert response.status_code == 200, response.text
+        session_data = response.json()
+        assert session_data['token_type'] is not None
+        assert session_data['access_token'] is not None
 
-    # POST ITEM
-    headers = {"Authorization":
-               f"{session_data['token_type']} {session_data['access_token']}"}
-    response = client.post(
-        settings.API_PREFIX+"/users/1/items/",
-        headers=headers,
-        json={"title": fake_name, "description": fake_description},
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data['id'] is not None
-    assert "id" in data
-    item_id = data["id"]
+        # TOKEN HEADERS
+        headers = {
+            "Authorization":
+            f"{session_data['token_type']} {session_data['access_token']}"}
+        self.headers = headers
 
-    # READ ITEMS
-    response = client.get(settings.API_PREFIX+"/items/",
-                          headers=headers,)
-    assert response.status_code == 200
+        # NEGATIVE TEST
+        self.wrong_id = 912093018209302910
 
-    # READ ITEM
-    response = client.get(settings.API_PREFIX+"/items/"+str(data['id']),
-                          headers=headers,)
-    assert response.status_code == 200
+    def test_create(self):
+        response = client.post(
+            settings.API_PREFIX+"/users/1/items/",
+            headers={"X-Token": "fake-super-secret-token"},
+            json={"title": fake_name, "description": fake_description},
+        )
+        assert response.status_code == 200, response.text
 
-    # UPDATE ITEM
-    response = client.put(
-        f"{settings.API_PREFIX}{local_prefix}{item_id}",
-        json={"title": fake_name_2, "description": fake_description},
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["title"] != fake_name
-    assert data["title"] == fake_name_2
+    @pytest.fixture(autouse=True)
+    def test_get(self):
+        # READ ITEMS
+        response = client.post(
+            settings.API_PREFIX+"/users/1/items/",
+            headers=self.headers,
+            json={"title": fake_name, "description": fake_description},
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data['id'] is not None
+        assert "id" in data
+        self.id_test = data['id']
 
-    # DELETE ITEM
-    response = client.delete(
-        f"{settings.API_PREFIX}{local_prefix}",
-        json={"id": item_id},
-    )
-    assert response.status_code == 200
+        response = client.get(settings.API_PREFIX+"/items/",
+                              headers=self.headers,)
+        assert response.status_code == 200
 
-    # NEGATIVE TEST
-    wrong_id = 90123801902930923  # just random id
+        # READ ITEM
+        response = client.get(settings.API_PREFIX+"/items/"+str(data['id']),
+                              headers=self.headers,)
+        assert response.status_code == 200
 
-    # UPDATE ITEM
-    response = client.put(
-        f"{settings.API_PREFIX}{local_prefix}{wrong_id}",
-        json={"title": fake_name_2, "description": fake_description},
-    )
-    assert response.status_code == 404, response.text
+    def test_update(self):
+        response = client.put(
+            f"{settings.API_PREFIX}{local_prefix}{self.id_test}",
+            json={"title": fake_name_2, "description": fake_description},
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["title"] != fake_name
+        assert data["title"] == fake_name_2
 
-    # DELETE ITEM
-    response = client.delete(
-        f"{settings.API_PREFIX}{local_prefix}",
-        json={"id": wrong_id},
-    )
-    assert response.status_code == 404
+    def test_delete(self):
+        response = client.delete(
+            f"{settings.API_PREFIX}{local_prefix}",
+            json={"id": self.id_test},
+        )
+        assert response.status_code == 200
 
-    # READ NOT EXIST ITEM
-    response = client.get(f"{settings.API_PREFIX}{local_prefix}{item_id}",
-                          headers=headers,)
-    assert response.status_code == 404
+    # ============ NEGATIVE TEST ============
+    def test_negative_get(self):
+        response = client.get(f"{settings.API_PREFIX}{local_prefix}" +
+                              f"{self.wrong_id}",
+                              headers=self.headers,)
+        assert response.status_code == 404
+
+    def test_negative_update(self):
+        response = client.put(
+            f"{settings.API_PREFIX}{local_prefix}{self.wrong_id}",
+            json={"title": fake_name_2, "description": fake_description},
+        )
+        assert response.status_code == 404, response.text
+
+    def test_negative_delete(self):
+        response = client.delete(
+            f"{settings.API_PREFIX}{local_prefix}",
+            json={"id": self.wrong_id},
+        )
+        assert response.status_code == 404
